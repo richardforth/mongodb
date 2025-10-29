02192cf20718f3680aec407a306045731f1228e6
Richard A. Forth <richard.forth@gmail.com>
2025-10-29 21:44:37 +0000

    Exploring mongodb out of the box

--- code/hour02/eval_expressions.sh
+++ code/hour02/eval_expressions.sh
@@ -6,8 +6,12 @@ mongosh --eval "show databases"
 echo -e "\ntest DB Collections:"
 mongosh test --eval "printjson(db.getCollectionNames())"
 
-echo -e "\nconfig DB Collections:"
-mongosh config --eval "printjson(db.getCollectionNames())"
 
 echo -e "\nadmin DB Collections:"
 mongosh admin --eval "printjson(db.getCollectionNames())"
+
+echo -e "\nconfig DB Collections:"
+mongosh config --eval "printjson(db.getCollectionNames())"
+
+echo -e "\nlocal DB Collections:"
+mongosh local --eval "printjson(db.getCollectionNames())"
